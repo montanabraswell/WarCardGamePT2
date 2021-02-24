8473700cbdf0740b33072bc2097c2e124d5d7e31
Montana <montana@Montanas-MacBook-Pro.local>
2021-02-23 20:01:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WarCardPT2
---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Montana  on 2/23/21.
+//  Copyright © 2021 Montana Braswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
