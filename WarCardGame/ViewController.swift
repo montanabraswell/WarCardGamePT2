fa1859993d158c1d342ab72cad1dfc41419c6124
Montana <montana@Montanas-MacBook-Pro.local>
2021-03-11 20:26:42 -0500

    Added logic to the cards and implemented score logic

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -15,27 +15,49 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightImageView: UIImageView!
     
-    
     @IBOutlet weak var leftScoreLabel: UILabel!
     
-    
     @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-       
-        
     }
 
     @IBAction func dealTapped(_ sender: Any) {
         
+        // Randomized some numbers
         let leftNumber = Int.random(in: 2...14)
         let rightNumber = Int.random(in: 2...14)
         
+        // Update the image views
         leftImageView.image = UIImage(named: "card\(leftNumber)")
         rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        // Compare the random numbers
+        if leftNumber > rightNumber {
+            
+            // Left side wins
+            leftScore += 1
+            
+            leftScoreLabel.text = String(leftScore)
+            
+        }
+        else if leftNumber < rightNumber {
+            
+            // Right side vins
+            rightScore += 1
+            
+            rightScoreLabel.text = String(rightNumber)
+            
+        }
+         else {
+            // Tie
+        }
     }
     
 }
